bbff041f7fab597ae2e25a55095097ec9d345b6d
jea68 <joshua.abu@yahoo.com>
2024-05-29 16:48:40 +0100

    firebase

--- yt-web-client/app/firebase/firebase.ts
+++ yt-web-client/app/firebase/firebase.ts
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth, signInWithPopup, GoogleAuthProvider, onAuthStateChanged, User } from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -19,3 +21,28 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth= getAuth(app);
+
+/**
+ * Signs the user in with a Google popup.
+ * @returns A promise that resolves with the user's credentials.
+ */
+export function signInWithGoogle() {
+    return signInWithPopup(auth, new GoogleAuthProvider()); // could use other providers
+  }
+  
+  /**
+   * Signs the user out.
+   * @returns A promise that resolves when the user is signed out.
+   */
+  export function signOut() {
+    return auth.signOut();
+  }
+  
+  /**
+   * Trigger a callback when user auth state changes.
+   * @returns A function to unsubscribe callback.
+   */
+  export function onAuthStateChangedHelper(callback: (user: User | null) => void) {
+    return onAuthStateChanged(auth, callback);
+  }
