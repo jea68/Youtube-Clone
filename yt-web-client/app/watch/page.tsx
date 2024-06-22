b65d0de1da7bb796845f94f99a7f7722ae0fec9c
jea68 <joshua.abu@yahoo.com>
2024-06-23 00:09:21 +0100

    Deploy Web Client to Cloud Run using Docker

--- yt-web-client/app/watch/page.tsx
+++ yt-web-client/app/watch/page.tsx
@@ -1,18 +1,29 @@
 'use client';
 
-import { useSearchParams } from 'next/navigation'
+import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
 
-export default function Watch() {
+const WatchContent = () => {
   const videoPrefix = 'https://storage.googleapis.com/jea68-yt-processed-videos/';
-  const videoSrc = useSearchParams().get('v');
+  const searchParams = useSearchParams();
+  const videoSrc = searchParams.get('v');
 
   return (
     <div>
       <h1>Watch Page</h1>
-      { <video controls src={videoPrefix + videoSrc}/> }
+      {videoSrc ? <video controls src={videoPrefix + videoSrc}/> : <p>Loading...</p>}
     </div>
   );
+};
+
+export default function Watch() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <WatchContent />
+    </Suspense>
+  );
 }
+
 
 
   // name of the page in url depends on name of function => file must eb called "page"
