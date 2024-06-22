d5b19612c5889b36d4f4827a6399d700bfc1690a
jea68 <joshua.abu@yahoo.com>
2024-06-22 14:20:24 +0100

    Creation of Google Cloud, Web App, and Firebase

---
+++ yt-api-service/functions/src/index.ts
@@ -0,0 +1,35 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * import {onCall} from "firebase-functions/v2/https";
+ * import {onDocumentWritten} from "firebase-functions/v2/firestore";
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ * THIS IS AN APInpm r
+ */
+
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+import * as logger from "firebase-functions/logger";
+import {Storage} from "@google-cloud/storage";
+import {onCall} from "firebase-functions/v2/https";
+
+initializeApp();
+const firestore = new Firestore();
+
+const storage = new Storage();
+const rawVideoBucketName = "jea68-yt-raw-videos";
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
+
--- yt-web-client/app/navbar/navbar.tsx
+++ yt-web-client/app/navbar/navbar.tsx
@@ -1,15 +1,39 @@
-import Image from "next/image";
+'use client';
+
+import SignIn from "./sign-in";
 import Link from "next/link";
 
 import styles from "./navbar.module.css";
+//import Upload from "./upload";
+import { useEffect, useState } from "react";
+import { onAuthStateChangedHelper } from "../firebase/firebase";
+import { User } from "firebase/auth";
 
-export default function Navbar() {
+
+function NavBar() {
+  // Initialize user state
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => { // react hook
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, [] /* No dependencies, never rerun */);
+
+
   return (
     <nav className={styles.nav}>
       <Link href="/">
-        <Image width={90} height={20}
-          src="/youtube-logo.svg" alt="YouTube Logo"/>
+        <span className={styles.logoContainer}>
+          <img className={styles.logo} src="/youtube-logo.svg" alt="YouTube Logo" />
+        </span>
       </Link>
+      <SignIn user={user} />
     </nav>
   );
 }
+
+export default NavBar;
